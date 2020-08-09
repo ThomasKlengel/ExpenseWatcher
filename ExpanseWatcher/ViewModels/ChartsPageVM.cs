d8139d513cbebbf052b851089fdeec4cd6dca79c
Thomas Klengel <thomas.klengel85@googlemail.copm>
2020-08-09 20:19:41 +0200

    Charts: added calculation for categories in pie chart

--- ExpanseWatcher/ViewModels/ChartsPageVM.cs
+++ ExpanseWatcher/ViewModels/ChartsPageVM.cs
@@ -15,30 +15,46 @@ namespace ExpanseWatcher.ViewModels
 
         public ChartsPageVM()
         {
-            
+
         }
 
         public SeriesCollection Series
         {
             get
             {
-                Dictionary<string, double> data =
-                    new Dictionary<string, double>() {};
-                data.Add("Essen", 3.3);
-                data.Add("Trinken", 1);
-                data.Add("Tische", 12.21);
                 var col = new SeriesCollection();
-                foreach (var entry in data)
+                // go through each category
+                List<Payment> payInCat = new List<Payment>();
+                foreach (var cat in Globals.Categories)
                 {
-                    var ps = new PieSeries();
-                    ps.Title = entry.Key;
-                    ps.Values = new ChartValues<Double>() {entry.Value } ;
-                    ps.LabelPoint = chartPoint =>
-                    string.Format($"{entry.Key} {chartPoint.Y} ({chartPoint.Participation:P})");
-                    ps.DataLabels = true;
-                    col.Add(ps);
+                    var series = new PieSeries();
+                    series.Title = cat.Name;
+                    // sum up the amount of payments to the category
+                    var sumPrice = 0.0;
+                    foreach (var shop in cat.AttachedShops)
+                    {
+                        Globals.Payments.Where(p => p.Shop == shop).ToList().ForEach(p => { sumPrice += p.Price; payInCat.Add(p); });
+                    }
+                    // add data to series collection
+                    series.Values = new ChartValues<Double>() { sumPrice };
+                    series.LabelPoint = chartPoint =>
+                    string.Format($"{cat.Name} {chartPoint.Y}€ ({chartPoint.Participation:P})");
+                    series.DataLabels = true;
+                    col.Add(series);
                 }
 
+
+                // sum up the amount of payments not in any category
+                var sum = 0.0;
+                Globals.Payments.Where(p => !payInCat.Contains(p)).ToList().ForEach(p => sum += p.Price);
+                // add data to series collection
+                var ps = new PieSeries();
+                ps.Title = "Other";
+                ps.Values = new ChartValues<Double>() { sum };
+                ps.LabelPoint = chartPoint =>
+                string.Format($"{"Other"} {chartPoint.Y}€ ({chartPoint.Participation:P})");
+                ps.DataLabels = true;
+                col.Add(ps);
 
                 return col;
             }
