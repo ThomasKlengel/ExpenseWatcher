d20d6ae95e96c3a2e147b17193270a9b8c9bb5b5
Thomas Klengel <thomas.klengel85@googlemail.copm>
2020-08-11 22:30:10 +0200

    * ChartsPage, ChartsPageVM: added stacked column series for payment per week * ExpenseOverview:  added string fromats for date and price

--- ExpanseWatcher/ViewModels/ChartsPageVM.cs
+++ ExpanseWatcher/ViewModels/ChartsPageVM.cs
@@ -58,9 +58,58 @@ namespace ExpanseWatcher.ViewModels
 
                 return col;
             }
-
         }
 
+        public SeriesCollection Series2
+        {
+            get
+            {
+                var col = new SeriesCollection();
+                // go through each category
+                List<Payment> payInCat = new List<Payment>();
+                foreach (var cat in Globals.Categories)
+                {
+                    var series = new StackedColumnSeries();
+                    series.Title = cat.Name;
+                    // sum up the amount of payments to the category
+                    List<object> sumPrices = new List<object>();
+                    for (int i = 12; i >= 0; i--)
+                    {
+                        var sumX = 0.0;
+                        foreach (var shop in cat.AttachedShops)
+                        {
+                            Globals.Payments.Where(p => p.Shop == shop 
+                                && p.DateOfPayment>DateTimeOffset.Now.AddDays((-i-1)*7)
+                                && p.DateOfPayment < DateTimeOffset.Now.AddDays(-i*7))
+                                .ToList().ForEach(p => { sumX += p.Price; });
+                        }
+                        sumPrices.Add(sumX);
+                    }
+                    // add data to series collection
+                    series.Values = new ChartValues<Double>();
+                    series.Values.AddRange(sumPrices);
+                    series.LabelPoint = chartPoint =>
+                    string.Format($"{chartPoint.Y}€");
+                    series.DataLabels = true;
+                    col.Add(series);
+                }
+
+
+                //// sum up the amount of payments not in any category
+                //var sum = 0.0;
+                //Globals.Payments.Where(p => !payInCat.Contains(p)).ToList().ForEach(p => sum += p.Price);
+                //// add data to series collection
+                //var ps = new PieSeries();
+                //ps.Title = "Other";
+                //ps.Values = new ChartValues<Double>() { sum };
+                //ps.LabelPoint = chartPoint =>
+                //string.Format($"{"Other"} {chartPoint.Y}€ ({chartPoint.Participation:P})");
+                //ps.DataLabels = true;
+                //col.Add(ps);
+
+                return col;
+            }
+        }
     }
 
     public class Data
