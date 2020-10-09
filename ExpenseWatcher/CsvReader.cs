d7c1d604ef3433b390c03ffe5679e7a5ac6abf5d
Thomas Klengel <thomas.klengel85@googlemail.copm>
2020-10-09 19:16:21 +0200

    * fixed minor bug in adding payments from csv

--- ExpenseWatcher/CsvReader.cs
+++ ExpenseWatcher/CsvReader.cs
@@ -96,12 +96,12 @@ namespace ExpanseWatcher
             int i = 0;
             foreach (var payment in newPayments)
             {
-                if (currentPayments.Any(curPay => curPay.TransactionCode == payment.TransactionCode))
+                if (currentPayments.Any(curPay => curPay.Equals(payment)))
                 {
                     Logging.Log.Info($"payment is already in database: {payment.Price}€, {payment.Shop}, {payment.DateOfPayment.ToString("yyyy-MM-dd HH:mm:ss")}");
                     continue;
                 }
-                while (currentPayments.Any(curPay => curPay.Equals(payment)))
+                while (currentPayments.Any(curPay => curPay.DateOfPayment == payment.DateOfPayment))
                 {
                     payment.DateOfPayment = payment.DateOfPayment.AddMinutes(1);
                 }
